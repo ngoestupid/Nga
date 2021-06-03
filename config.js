7fc17f6acb6be50c34efb2c8f420402a39086a51
ngoestupid <85279776+ngoestupid@users.noreply.github.com>
2021-06-03 14:51:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'anh yêu em lắm bé à',
+    desc: 'anh nhớ em lắm bé ạ ',
+    btnYes: 'em cũng nhớ a mòa <33',
+    btnNo: 'tao đang giận mày đấy tml',
+    question:'đừng giận dỗi a nữa nha yêu em nhiều ',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'vâng ạ yêu yêu',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'đừng giận a nữa nha',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'dạ <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }

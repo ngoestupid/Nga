815026d849a9f174ccd2d3a7da9315d4f592f425
ngoestupid <85279776+ngoestupid@users.noreply.github.com>
2023-07-05 18:59:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Hí lô hâm!',
+    introTitle: 'Hí Nga iu dấu!',
     introDesc: `Trái đất vốn lạ thường
-    mà sao B cứ đi nhầm đường
-    lạc vào tim t lẻ loi`,
+    mà sao em cứ đi nhầm đường
+    lạc vào tim anh lẻ loi`,
     btnIntro: 'Haha',
-    title: 'B có thích mình hông <3',
-    desc: 'hãy chọn đi ',
-    btnYes: 'mình thích b mất rồi <33',
+    title: 'Em có thích anh hông <3',
+    desc: 'Em hãy chọn đi ',
+    btnYes: 'Em thích anh mất rồi <33',
     btnNo: 'không (đừng chọn cái này )))):)',
-    question:'ahihi mình biết câu trả lời rồi nha ',
-    btnReply: 'B định nói gì nào',
-    reply: 'quá đỉnh luôn',
-    mess: 'Mình biết mà 🥰. B cứ khen thế mình ngại lắm ý 😘😘',
+    question:'ahihi Anh biết câu trả lời rồi nha ',
+    btnReply: 'Em định nói gì nào',
+    reply: 'Em yêu anh',
+    mess: 'Anh biết mà 🥰. Em cứ bị đáng yêu ý 😘😘',
     messDesc: 'hí hí yêu quá đi mà',
-    btnAccept: 'hihi <3',
+    btnAccept: 'Yêu em <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
